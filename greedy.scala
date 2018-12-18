e0dac02226f0662f13dcf717433a952a0cd79e65
PreetrajSGujral <44151163+RUGujral@users.noreply.github.com>
2018-12-18 06:51:10 -0500

    Update greedy.scala

--- greedy.scala
+++ greedy.scala
@@ -12,26 +12,22 @@ def greedy[VD](g:Graph[VD,Double], origin:VertexId) = {
  var edgesAreAvailable = true
  do {
  type tripletType = EdgeTriplet[Boolean,Tuple2[Double,Boolean]]
- val availableEdges =g2.triplets
- .filter(et => !et.attr._2
- && (et.srcId == nextVertexId && !et.dstAttr
- || et.dstId == nextVertexId && !et.srcAttr))
+ val availableEdges =g2.triplets.filter(et => !et.attr._2 && (et.srcId == nextVertexId && !et.dstAttr || et.dstId == nextVertexId && !et.srcAttr))
+ 
  edgesAreAvailable = availableEdges.count > 0
+ 
  if (edgesAreAvailable) {
- val smallestEdge = availableEdges
- .min()(new Ordering[tripletType]() {
- override def compare(a:tripletType, b:tripletType) = {
- Ordering[Double].compare(a.attr._1,b.attr._1)
- }
- })
- nextVertexId = Seq(smallestEdge.srcId, smallestEdge.dstId)
- .filter(_ != nextVertexId)(0)
- g2 = g2.mapVertices((vid,vd) => vd || vid == nextVertexId)
- .mapTriplets(et => (et.attr._1,
- et.attr._2 ||
- (et.srcId == smallestEdge.srcId
- && et.dstId == smallestEdge.dstId)))
- }
+  val smallestEdge = availableEdges.min() (new Ordering[tripletType]() {
+   override def compare(a:tripletType, b:tripletType) = {
+    Ordering[Double].compare(a.attr._1,b.attr._1)
+    }
+   })
+  
+ nextVertexId = Seq(smallestEdge.srcId, smallestEdge.dstId).filter(_ != nextVertexId)(0)
+
+ g2 = g2.mapVertices((vid,vd) => vd || vid == nextVertexId).mapTriplets(et => (et.attr._1, et.attr._2 ||
+      (et.srcId == smallestEdge.srcId && et.dstId == smallestEdge.dstId)))
+ } 
  } while(edgesAreAvailable)
  g2
 }
